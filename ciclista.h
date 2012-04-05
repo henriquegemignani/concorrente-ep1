a196aa8c4720ee5f0fb58e41aa94283009266d12
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-04-05 14:47:01 -0300

    Arquivos iniciais.

---
+++ ciclista.h
@@ -0,0 +1,12 @@
+#ifndef CICLISTA_H_
+#define CICLISTA_H_
+
+typedef struct Ciclista {
+    char *nome;
+    double metros;
+    double vel_plano;
+    double vel_subida;
+    double vel_descida;
+} ciclista;
+
+#endif /* CICLISTA_H_ */
